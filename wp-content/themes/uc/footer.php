6153194d76f09cad75bad2ad4a6743ba0e194dfc
wuhulinyi@sina.com <wuhulinyi@sina.com>
2016-11-14 18:10:27 +0800

    modify foot

--- wp-content/themes/uc/footer.php
+++ wp-content/themes/uc/footer.php
@@ -1,6 +1,6 @@
 <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 <footer class="mod-footer" role="contentinfo" id="footer_in">
-    <p class="f_bq"> <?php if($word_t2!=""){echo $word_t2;}else{echo 'Copyright';}  ?> &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> <span style="color: #524545;">京ICP备16054701号-1</span></p>
+    <p class="f_bq"> <?php if($word_t2!=""){echo $word_t2;}else{echo 'Copyright';}  ?> &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?> <span id="icp" style="color: #524545;">京ICP备16054701号-1</span></p>
 </footer>
 <?php wp_footer(); ?>
 <script type="application/ld+json">
